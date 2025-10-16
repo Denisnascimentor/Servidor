0eb8c1a1c4fab3260c20e759d0f62f652e99525c
mariana <eumarianamota@gmail.com>
2025-10-16 17:14:10 -0300

    feat: add server classes

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+let nameSection = document.getElementById("name-section")
+let mainSection = document.getElementById("main-section")
+
+function enterClass() {
+    nameSection.classList.remove('active')
+    mainSection.classList.add('active')
+}
